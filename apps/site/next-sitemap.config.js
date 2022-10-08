37ba0d825bbb9386f91a0f55de6c002dd0d4f1db
Or Druker <or@druker.net>
2022-10-08 23:38:31 +0300

    stop auto-generating server-sitemap.xml

--- apps/site/next-sitemap.config.js
+++ apps/site/next-sitemap.config.js
@@ -1,22 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.druker.net',
-  generateRobotsTxt: true, // (optional)
-  changefreq: 'daily',
-  priority: 0.7,
-  // ...other options
-};
-
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.druker.net';
 
 module.exports = {
   siteUrl: siteUrl,
+  exclude: ['/404'],
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  robotsTxtOptions: {
-    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
-  },
+  // robotsTxtOptions: {
+  //   additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
+  // },
   sourceDir: 'dist/apps/site/.next',
   outDir: 'dist/apps/site/public',
 };
