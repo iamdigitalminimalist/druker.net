6a9763e7b7d25fe1dd84a436e645eeba5807a440
Or Druker <or@druker.net>
2022-10-10 17:16:40 +0300

    remove nextjs-google-analytics lib

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,12 +1,8 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import './styles.css';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
-import Script from 'next/script';
 import { useEffect } from 'react';
 import TagManager, { TagManagerArgs } from 'react-gtm-module';
-
-// const isProduction = process.env.NODE_ENV === 'production';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   // Google Tag Manager
