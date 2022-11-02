0a20e2379b0565649a87004709efc0f27a075b7e
Or Druker <or@druker.net>
2022-11-02 15:11:55 +0200

    fix an issue with the mdx Link component

--- libs/shared/mdx-elements/src/lib/custom-link/custom-link.tsx
+++ libs/shared/mdx-elements/src/lib/custom-link/custom-link.tsx
@@ -3,18 +3,22 @@ import Link from 'next/link';
 
 /* eslint-disable-next-line */
 export interface CustomLinkProps {
+  children: React.ReactNode
   as: string;
   href: string;
 }
 
-export function CustomLink({ as, href, ...otherProps }: CustomLinkProps) {
+export function CustomLink({ as, href,children, ...otherProps }: CustomLinkProps) {
   return (
     <Link as={as} href={href}>
       <a
         target="_blank"
+        rel="noopener noreferrer"
         className="no-underline transition ease-in-out duration-300 hover:opacity-80 hover:underline"
         {...otherProps}
-      />
+      >
+        {children}
+      </a>
     </Link>
   );
 }
